8010432a161df1e1a1d12d44dbac56e7525fa6d4
chuanzong.lcz <chuanzong.lcz@alibaba-inc.com>
2018-11-19 01:40:23 +0800

    feat: food detail page

--- lib/food_detail.dart
+++ lib/food_detail.dart
@@ -26,34 +26,115 @@ class FoodModel {
   final String name;
   final StoreModel store;
   final int orderedCount;
-  final ImageModel image;
+  final List<ImageModel> images;
 
-  FoodModel({ @required this.name, @required this.store, @required this.orderedCount, @required this.image });
+  FoodModel({ @required this.name, @required this.store, @required this.orderedCount, @required this.images });
 
   factory FoodModel.fromJson(Map<String, dynamic> foodJson) {
     return FoodModel(
       name: foodJson['name'],
       orderedCount: foodJson['ordered_count'],
       store: StoreModel.fromJson(foodJson['store']),
-      image: ImageModel.fromJson(foodJson['image']),
+      images: List.from(foodJson['images']).map((image) => ImageModel.fromJson(image)).toList(),
     );
   }
 }
 
 class FoodDetail extends StatelessWidget {
+  final FoodModel food;
+
   FoodDetail(this.food);
 
-  final FoodModel food;
+  static Widget _renderImages(List<ImageModel> images) {
+    Widget gallery;
+
+    // Make single image cover the screen
+    if (images.length <= 1) {
+      gallery = GestureDetector(
+        onTap: () {
+          print('image ${images[0]} tapped!');
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: new NetworkImage(images[0].src),
+              fit: BoxFit.cover,
+            ),
+
+            borderRadius: BorderRadius.all(const Radius.circular(5))
+          ),
+        )
+      );
+    } else {
+      // Make multiple images scrolled horizontally
+      gallery = ListView(
+        scrollDirection: Axis.horizontal,
+        children: images.map((img) {
+          return Container(
+            padding: EdgeInsets.only(right: 4),
+
+            child: new GestureDetector(
+              onTap: () {
+                print('$img tapped!');
+              },
+              child: Container(
+                child: ClipRRect(
+                  child: Image.network(img.src, fit: BoxFit.cover),
+                  borderRadius: BorderRadius.all(const Radius.circular(5)),
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      );
+    }
+
+    return Container(
+      height: 350,
+      child: gallery,
+    );
+  }
+
+  static Widget _renderDetail(FoodModel food) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      child: Center(
+        child: Text(food.name),
+      ),
+    );
+  }
+
+  static Widget _renderActions(FoodModel food) {
+    return Container(
+      height: 40,
+      margin: EdgeInsets.fromLTRB(0, 40, 4, 0),
+      child: RaisedButton(
+        onPressed: () {
+          print('goto order');
+        },
+        color: Color(0xffd4edda),
+//        color: Color(0xffc3e6cb),
+        child: Text('下单'),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('food.images ${food.images.length}');
+    
     return Scaffold(
       appBar: AppBar(
         title: Text(food.name),
       ),
-      body: Center(
-        child: Text('Hello World', style: TextStyle(fontSize: 32.0)),
-      ),
+      body: ListView(
+        padding: EdgeInsets.fromLTRB(4, 4, food.images.length > 1 ? 0 : 4, 4),
+        children: <Widget>[
+          _renderImages(food.images),
+          _renderDetail(food),
+          _renderActions(food),
+        ],
+      )
     );
   }
 }
--- lib/foods.dart
+++ lib/foods.dart
@@ -13,52 +13,68 @@ const List<Map<String, dynamic>> foods = [
       'name': '法式面包店',
       'distance': '1.2 km',
     },
-    'image': {
-      'src': 'http://img.hb.aicdn.com/f34c2c2b70d2d3aacdf0e3864aad0f6969e53826edd03-NBEyh5_fw658',
-      'ratio': 0.5
-    },
+    'images': [
+      {
+        'src': 'http://img.hb.aicdn.com/f34c2c2b70d2d3aacdf0e3864aad0f6969e53826edd03-NBEyh5_fw658',
+        'ratio': 0.66,
+      },
+      {
+        'src': 'http://img.hb.aicdn.com/4b24590fc729b83e4ca01a7215732210fe4e931e10c2d-yFGmAO_fw658',
+        'ratio': 0.66,
+      },
+      {
+        'src': 'http://img.hb.aicdn.com/0a8d56cfc7967ef633db3c3b1883942807ede4a537474-di3cRx_fw658',
+        'ratio': 0.66,
+      },
+    ],
   },
   {
-    'name': 'Chocolate Bread Bums',
+    'name': 'Breads',
     'ordered_count': 15,
     'store': {
       'name': '法式面包店',
       'distance': '1.1 km',
     },
-    'image': {
-      'src': 'http://img.hb.aicdn.com/fa204e95db0881507a41bbe877be06599558850e6f1b5-LENt5Z_fw658',
-      'ratio': 1,
-    },
+    'images': [
+      {
+        'src': 'http://img.hb.aicdn.com/fa204e95db0881507a41bbe877be06599558850e6f1b5-LENt5Z_fw658',
+        'ratio': 1,
+      }
+    ],
   },
   {
-    'name': 'Chocolate Bread Bums',
+    'name': 'Tea & Bread',
     'ordered_count': 15,
     'store': {
       'name': '法式面包店',
       'distance': '2.0 km',
     },
-    'image': {
-      'src': 'http://img.hb.aicdn.com/a72e8a87c6a88c3a10a93202a5a1f0e9b9c6ac293fe90-3Xes2R_fw658',
-      'ratio': 0.5,
-    },
+    'images': [
+      {
+        'src': 'http://img.hb.aicdn.com/a72e8a87c6a88c3a10a93202a5a1f0e9b9c6ac293fe90-3Xes2R_fw658',
+        'ratio': 0.66,
+      }
+    ],
   },
   {
-    'name': 'Chocolate Bread Bums',
+    'name': 'Bread with Melt Chocolate',
     'ordered_count': 15,
     'store': {
       'name': '法式面包店',
       'distance': '2.0 km',
     },
-    'image': {
-      'src': 'http://img.hb.aicdn.com/f6a639629b29724a1e2759e1e5c34c1c8b1d1f71656aa-ldSnEW_fw658',
-      'ratio': 0.5,
-    },
+    'images': [
+      {
+        'src': 'http://img.hb.aicdn.com/f6a639629b29724a1e2759e1e5c34c1c8b1d1f71656aa-ldSnEW_fw658',
+        'ratio': 0.66,
+      }
+    ],
   },
 ];
 
 List<StaggeredTile> _staggeredTiles = foods
   .map((food) {
-    final ratio = food['image']['ratio'] ?? 0.5;
+    final ratio = food['images'][0]['ratio'] ?? 0.5;
     final height = (1 / ratio).round();
 
     return StaggeredTile.count(1, height);
@@ -77,7 +93,7 @@ class _FoodTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final FoodModel food = FoodModel.fromJson(this.food);
-    final src = food.image.src;
+    final src = food.images.elementAt(0).src;
 
     return new Card(
       color: const Color(0x00000000),
@@ -105,8 +121,8 @@ class _FoodTile extends StatelessWidget {
 class Foods extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Padding(
-      padding: const EdgeInsets.only(top: 12.0),
+    return new Container(
+      padding: const EdgeInsets.only(top: 0),
       child: new StaggeredGridView.count(
         crossAxisCount: 2,
         staggeredTiles: _staggeredTiles,
